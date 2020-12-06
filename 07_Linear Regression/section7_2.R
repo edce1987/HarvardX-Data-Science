5e2b35dc4933e576daec692b2c6f63ec62a48934
edce1987 <44676946+edce1987@users.noreply.github.com>
2020-12-06 19:02:26 +0100

    Finished section 2

--- 07_Linear Regression/section7_2.R
+++ 07_Linear Regression/section7_2.R
@@ -474,3 +474,345 @@ options(digits = 3)
 A <- galton %>% 
   group_by(pair) %>% 
   do(tidy(lm(childHeight ~ parentHeight, data = .), conf.int = T))
+
+##Building better Offensive Metric
+# linear regression with two variables
+library(tidyverse)
+library(Lahman)
+library(broom)
+fit <- Teams %>% 
+  filter(yearID %in% 1961:2001) %>% 
+  mutate(BB = BB/G, HR = HR/G,  R = R/G) %>%  
+  lm(R ~ BB + HR, data = .)
+tidy(fit, conf.int = TRUE)
+
+# regression with BB, singles, doubles, triples, HR
+fit <- Teams %>% 
+  filter(yearID %in% 1961:2001) %>% 
+  mutate(BB = BB / G, 
+         singles = (H - X2B - X3B - HR) / G, 
+         doubles = X2B / G, 
+         triples = X3B / G, 
+         HR = HR / G,
+         R = R / G) %>%  
+  lm(R ~ BB + singles + doubles + triples + HR, data = .)
+coefs <- tidy(fit, conf.int = TRUE)
+coefs
+
+# predict number of runs for each team in 2002 and plot
+Teams %>% 
+  filter(yearID %in% 2002) %>% 
+  mutate(BB = BB/G, 
+         singles = (H-X2B-X3B-HR)/G, 
+         doubles = X2B/G, 
+         triples =X3B/G, 
+         HR=HR/G,
+         R=R/G)  %>% 
+  mutate(R_hat = predict(fit, newdata = .)) %>%
+  ggplot(aes(R_hat, R, label = teamID)) + 
+  geom_point() +
+  geom_text(nudge_x=0.1, cex = 2) + 
+  geom_abline()
+
+# average number of team plate appearances per game
+pa_per_game <- Batting %>% filter(yearID == 2002) %>% 
+  group_by(teamID) %>%
+  summarize(pa_per_game = sum(AB+BB)/max(G)) %>% 
+  pull(pa_per_game) %>% 
+  mean
+
+# compute per-plate-appearance rates for players available in 2002 using previous data
+players <- Batting %>% filter(yearID %in% 1999:2001) %>% 
+  group_by(playerID) %>%
+  mutate(PA = BB + AB) %>%
+  summarize(G = sum(PA)/pa_per_game,
+            BB = sum(BB)/G,
+            singles = sum(H-X2B-X3B-HR)/G,
+            doubles = sum(X2B)/G, 
+            triples = sum(X3B)/G, 
+            HR = sum(HR)/G,
+            AVG = sum(H)/sum(AB),
+            PA = sum(PA)) %>%
+  filter(PA >= 300) %>%
+  select(-G) %>%
+  mutate(R_hat = predict(fit, newdata = .))
+
+# plot player-specific predicted runs
+qplot(R_hat, data = players, geom = "histogram", binwidth = 0.5, color = I("black"))
+
+# add 2002 salary of each player
+players <- Salaries %>% 
+  filter(yearID == 2002) %>%
+  select(playerID, salary) %>%
+  right_join(players, by="playerID")
+
+# add defensive position
+position_names <- c("G_p","G_c","G_1b","G_2b","G_3b","G_ss","G_lf","G_cf","G_rf")
+tmp_tab <- Appearances %>% 
+  filter(yearID == 2002) %>% 
+  group_by(playerID) %>%
+  summarize_at(position_names, sum) %>%
+  ungroup()  
+pos <- tmp_tab %>%
+  select(position_names) %>%
+  apply(., 1, which.max) 
+players <- data_frame(playerID = tmp_tab$playerID, POS = position_names[pos]) %>%
+  mutate(POS = str_to_upper(str_remove(POS, "G_"))) %>%
+  filter(POS != "P") %>%
+  right_join(players, by="playerID") %>%
+  filter(!is.na(POS)  & !is.na(salary))
+
+# add players' first and last names
+players <- Master %>%
+  select(playerID, nameFirst, nameLast, debut) %>%
+  mutate(debut = as.Date(debut)) %>%
+  right_join(players, by="playerID")
+
+# top 10 players
+players %>% select(nameFirst, nameLast, POS, salary, R_hat) %>% 
+  arrange(desc(R_hat)) %>% 
+  top_n(10) 
+
+# players with a higher metric have higher salaries
+players %>% ggplot(aes(salary, R_hat, color = POS)) + 
+  geom_point() +
+  scale_x_log10()
+
+# remake plot without players that debuted after 1998
+library(lubridate)
+players %>% filter(year(debut) < 1998) %>%
+  ggplot(aes(salary, R_hat, color = POS)) + 
+  geom_point() +
+  scale_x_log10()
+
+##Excursion
+#A way to actually pick the players for the team can be done using what computer scientists call linear programming. Although we don't go into this topic in detail in this course, we include the code anyway:
+install.packages("reshape2")
+install.packages("lpSolve")
+library(reshape2)
+library(lpSolve)
+
+players <- players %>% filter(debut <= "1997-01-01" & debut > "1988-01-01")
+constraint_matrix <- acast(players, POS ~ playerID, fun.aggregate = length)
+npos <- nrow(constraint_matrix)
+constraint_matrix <- rbind(constraint_matrix, salary = players$salary)
+constraint_dir <- c(rep("==", npos), "<=")
+constraint_limit <- c(rep(1, npos), 50*10^6)
+lp_solution <- lp("max", players$R_hat,
+                  constraint_matrix, constraint_dir, constraint_limit,
+                  all.int = TRUE) 
+
+our_team <- players %>%
+  filter(lp_solution$solution == 1) %>%
+  arrange(desc(R_hat))
+our_team %>% select(nameFirst, nameLast, POS, salary, R_hat)
+
+my_scale <- function(x) (x - median(x))/mad(x)
+players %>% mutate(BB = my_scale(BB), 
+                   singles = my_scale(singles),
+                   doubles = my_scale(doubles),
+                   triples = my_scale(triples),
+                   HR = my_scale(HR),
+                   AVG = my_scale(AVG),
+                   R_hat = my_scale(R_hat)) %>%
+  filter(playerID %in% our_team$playerID) %>%
+  select(nameFirst, nameLast, BB, singles, doubles, triples, HR, AVG, R_hat) %>%
+  arrange(desc(R_hat))
+
+##OPS
+#The on-base-percentage plus slugging percentage (OPS) metric is:
+#  BBPA+(Singles+2Doubles+3Triples+4HR)AB
+
+#Key points
+#Regression can bring about errors in reasoning, especially when interpreting individual observations.
+#The example showed in the video demonstrates that the "sophomore slump" observed in the data is caused by regressing to the mean.
+
+library(Lahman)
+playerInfo <- Fielding %>%
+  group_by(playerID) %>%
+  arrange(desc(G)) %>%
+  slice(1) %>%
+  ungroup %>%
+  left_join(Master, by="playerID") %>%
+  select(playerID, nameFirst, nameLast, POS)
+
+ROY <- AwardsPlayers %>%
+  filter(awardID == "Rookie of the Year") %>%
+  left_join(playerInfo, by="playerID") %>%
+  rename(rookie_year = yearID) %>%
+  right_join(Batting, by="playerID") %>%
+  mutate(AVG = H/AB) %>%
+  filter(POS != "P")
+
+ROY <- ROY %>%
+  filter(yearID == rookie_year | yearID == rookie_year+1) %>%
+  group_by(playerID) %>%
+  mutate(rookie = ifelse(yearID == min(yearID), "rookie", "sophomore")) %>%
+  filter(n() == 2) %>%
+  ungroup %>%
+  select(playerID, rookie_year, rookie, nameFirst, nameLast, AVG)
+
+ROY <- ROY %>% spread(rookie, AVG) %>% arrange(desc(rookie))
+ROY
+
+mean(ROY$sophomore - ROY$rookie <= 0)
+
+two_years <- Batting %>%
+  filter(yearID %in% 2013:2014) %>%
+  group_by(playerID, yearID) %>%
+  filter(sum(AB) >= 130) %>%
+  summarize(AVG = sum(H)/sum(AB)) %>%
+  ungroup %>%
+  spread(yearID, AVG) %>%
+  filter(!is.na(`2013`) & !is.na(`2014`)) %>%
+  left_join(playerInfo, by="playerID") %>%
+  filter(POS!="P") %>%
+  select(-POS) %>%
+  arrange(desc(`2013`)) %>%
+  select(nameFirst, nameLast, `2013`, `2014`)
+two_years
+
+arrange(two_years, `2013`)
+
+qplot(`2013`, `2014`, data = two_years)
+
+summarize(two_years, cor(`2013`,`2014`))
+
+##Error modeling
+#Key points
+#Up to now, all our linear regression examples have been applied to two or more random variables. We assume the pairs are bivariate normal and use this to motivate a linear model.
+#Another use for linear regression is with measurement error models, where it is common to have a non-random covariate (such as time). Randomness is introduced from measurement error rather than sampling or natural variability.
+
+library(dslabs)
+falling_object <- rfalling_object()
+
+falling_object %>%
+  ggplot(aes(time, observed_distance)) +
+  geom_point() +
+  ylab("Distance in meters") +
+  xlab("Time in seconds")
+
+fit <- falling_object %>%
+  mutate(time_sq = time^2) %>%
+  lm(observed_distance~time+time_sq, data=.)
+
+tidy(fit)
+
+?augment
+
+augment(fit) %>%
+  ggplot() +
+  geom_point(aes(time, observed_distance)) +
+  geom_line(aes(time, .fitted), col = "blue")
+
+tidy(fit, conf.int = TRUE)
+
+##Assessment1
+#Q2
+pa_per_game <- Batting %>% 
+  filter(yearID == 2002) %>% 
+  group_by(teamID) %>%
+  summarize(pa_per_game = sum(AB+BB)/max(G)) %>% 
+  .$pa_per_game %>% 
+  mean
+
+#Assessment2
+
+library(Lahman)
+
+#Q9a
+data <- Teams %>% filter(yearID == 1971) %>%
+  lm(R ~ BB + HR, data=.)
+
+tidy(data)
+
+#Q10
+data2 <- Teams %>%  
+  group_by(yearID) %>%
+  do(tidy(lm(R ~ BB + HR, data = .), conf.int = TRUE)) %>%
+  filter(yearID %in% 1961:2018 & term == "BB")
+
+data2 %>% ggplot(aes(yearID, estimate)) + geom_point() + geom_smooth()
+View(data2)
+
+#Q11
+data2 <- Teams %>%  
+  group_by(yearID) %>%
+  do(tidy(lm(R ~ BB + HR, data = .), conf.int = TRUE)) %>%
+  filter(yearID %in% 1961:2018 & term == "BB")
+
+data2 %>% lm(estimate ~ yearID, data=.) %>% summary
+
+##Comprehensive Assessment
+library(tidyverse)
+library(broom)
+library(Lahman)
+Teams_small <- Teams %>% 
+  filter(yearID %in% 1961:2001) %>% 
+  mutate(avg_attendance = attendance/G, RPG = R/G, HRPG = HR/G)
+
+#1a
+Teams_small %>% 
+  do(tidy(lm(avg_attendance ~ RPG, data=.))) %>% 
+  filter(term=="RPG")
+
+Teams_small %>% 
+  do(tidy(lm(avg_attendance ~ HRPG, data=.))) %>% 
+  filter(term=="HRPG")
+
+#1b
+Teams_small %>% 
+  do(tidy(lm(avg_attendance ~ W, data=.)))
+
+#1c
+Teams_small %>% 
+  do(tidy(lm(avg_attendance ~ yearID, data=.)))
+
+#2
+Teams_small %>%
+  mutate(correlation=cor(W, HRPG)) %>% select(correlation) %>% summarise(corr=mean(correlation))
+
+#3a
+Teams_small %>% 
+  mutate(W10 = round(W/10,0)) %>%
+  group_by(W10) %>%
+  summarise(n=n()) %>% filter(n>=20)
+  
+#3b 
+Teams_small %>% 
+  mutate(W10 = round(W/10,0)) %>%
+  group_by(W10) %>%
+  mutate(n=n()) %>%
+  filter(n>=20) %>% do(tidy(lm(avg_attendance ~ RPG, data = .), conf.int = TRUE)) %>%
+  filter(term=="RPG")
+
+Teams_small %>% 
+  mutate(W10 = round(W/10,0)) %>%
+  group_by(W10) %>%
+  mutate(n=n()) %>%
+  filter(n>=20) %>% do(tidy(lm(avg_attendance ~ HRPG, data = .), conf.int = TRUE)) %>%
+  filter(term=="HRPG")
+
+#3C
+Teams_small %>% 
+  mutate(W10 = round(W/10,0)) %>%
+  group_by(W10) %>%
+  mutate(n=n()) %>%
+  filter(n>=20) %>% do(tidy(lm(avg_attendance ~ RPG, data = .), conf.int = TRUE)) %>%
+  filter(term=="RPG")
+
+#4
+Teams_small %>% lm(avg_attendance ~ RPG + HRPG + W + yearID, data = .) %>% tidy
+
+#5
+Teams_small %>% lm(avg_attendance ~ RPG + HRPG + W + yearID, data = .) %>% predict(data.frame(RPG = 5, HRPG = 1.2, W = 80, yearID = 1960))
+
+#6
+newdata <- Teams %>%
+  filter(yearID == 2002) %>%
+  mutate(avg_attendance = attendance/G,
+         R_per_game = R/G,
+         HR_per_game = HR/G)
+preds <- predict(fit, newdata)
+cor(preds, newdata$avg_attendance)
+
