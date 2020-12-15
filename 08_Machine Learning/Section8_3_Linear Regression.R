966640ea68b817a3315ff281b1056f32ecdb227c
edce1987 <44676946+edce1987@users.noreply.github.com>
2020-12-15 18:34:49 +0100

    Section 3 Smoothing

---
+++ 08_Machine Learning/Section8_3_Linear Regression.R
@@ -0,0 +1,358 @@
+#Key points
+#Linear regression can be considered a machine 
+#learning algorithm. Although it can be too rigid 
+#to be useful, it works rather well for some 
+#challenges. It also serves as a baseline 
+#approach: if you can’t beat it with a more complex 
+#approach, you probably want to stick to linear 
+#regression. 
+
+library(tidyverse)
+library(HistData)
+
+galton_heights <- GaltonFamilies %>%
+  filter(childNum == 1 & gender == "male") %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+
+library(caret)
+
+y <- galton_heights$son
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+
+train_set <- galton_heights %>% slice(-test_index)
+test_set <- galton_heights %>% slice(test_index)
+
+avg <- mean(train_set$son)
+avg
+
+mean((avg - test_set$son)^2)
+
+# fit linear regression model
+fit <- lm(son ~ father, data = train_set)
+fit$coef
+summary(fit)
+
+y_hat <- fit$coef[1] + fit$coef[2]*test_set$father
+mean((y_hat - test_set$son)^2)
+
+####
+
+#Key points
+#The predict() function takes a fitted 
+#object from functions such as 
+#lm() or glm() and a data frame with the new 
+#predictors for which to predict. We can use 
+#predict like this:
+y_hat <- predict(fit, test_set)
+
+#predict() is a generic function in R that 
+#calls other functions depending on what kind 
+#of object it receives. To learn about the 
+#specifics, you can read the help files using 
+#code like this: 
+?predict.lm    # or ?predict.glm
+
+y_hat <- predict(fit, test_set)
+mean((y_hat - test_set$son)^2)
+
+# read help files
+?predict.lm
+?predict.glm
+
+####Assessment
+options(digits=3)
+# set.seed(1) # if using R 3.5 or earlier
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+
+#Q1
+n <- 100
+x <- dat$x
+y <- dat$y
+set.seed(1, sample.kind="Rounding")
+sim <- replicate(n, {
+  test_index <- createDataPartition(y , times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  rmse <- sqrt(mean((y_hat-test_set$y)^2))
+})
+mean(sim)
+sd(sim)
+
+#Q2
+n <- c(100, 500, 1000, 5000, 10000)
+gen <- function(n) {
+  Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+  dat <- MASS::mvrnorm(n, c(69, 69), Sigma) %>% data.frame() %>% setNames(c("x", "y"))
+  sim <- replicate(100, {
+    test_index <- createDataPartition(dat$y , times = 1, p = 0.5, list = FALSE)
+    train_set <- dat %>% slice(-test_index)
+    test_set <- dat %>% slice(test_index)
+    fit <- lm(y ~ x, data = train_set)
+    y_hat <- predict(fit, newdata = test_set)
+    rmse <- sqrt(mean((y_hat-test_set$y)^2))
+  })
+  output <- c(mean(sim), sd(sim))
+}
+
+set.seed(1, sample.kind="Rounding")
+res <- sapply(n,gen)
+res
+
+#Q4
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.95, 0.95, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+
+n <- 100
+x <- dat$x
+y <- dat$y
+set.seed(1, sample.kind="Rounding")
+sim <- replicate(n, {
+  test_index <- createDataPartition(y , times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  rmse <- sqrt(mean((y_hat-test_set$y)^2))
+})
+mean(sim)
+sd(sim)
+
+#Q6
+
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+set.seed(1, sample.kind="Rounding")
+test_index <- createDataPartition(dat$y , times = 1, p = 0.5, list = FALSE)
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+y <- dat$y
+x_1 <- dat$x_1
+x_2 <- dat$x_2
+fit <- lm(y ~ x_1 + x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2)) #RMSE
+
+#Q8
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.95, 0.75, 0.95, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
+set.seed(1, sample.kind="Rounding")
+
+####Categorical Outcomes
+options(digits=3)
+library(tidyverse)
+library(dslabs)
+library(caret)
+
+data("heights")
+y <- heights$height
+
+set.seed(2, sample.kind = "Rounding") #if you are using R 3.6 or later
+
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+train_set <- heights %>% slice(-test_index)
+test_set <- heights %>% slice(test_index)
+
+train_set %>% 
+  filter(round(height)==66) %>%
+  summarize(y_hat = mean(sex=="Female"))
+
+heights %>% 
+  mutate(x = round(height)) %>%
+  group_by(x) %>%
+  filter(n() >= 10) %>%
+  summarize(prop = mean(sex == "Female")) %>%
+  ggplot(aes(x, prop)) +
+  geom_point()
+lm_fit <- mutate(train_set, y = as.numeric(sex == "Female")) %>% lm(y ~ height, data = .)
+p_hat <- predict(lm_fit, test_set)
+y_hat <- ifelse(p_hat > 0.5, "Female", "Male") %>% factor()
+confusionMatrix(y_hat, test_set$sex)$overall["Accuracy"]
+
+####Logistic Regression
+#Key points
+#Logistic regression is an extension of linear regression that assures that the estimate of conditional probability  Pr(Y=1|X=x)  is between 0 and 1. This approach makes use of the logistic transformation: 
+#  g(p)=logp1−p 
+#With logistic regression, we model the conditional probability directly with:
+#  g{Pr(Y=1|X=x)}=β0+β1x 
+#Note that with this model, we can no longer use least squares. Instead we compute the maximum likelihood estimate (MLE). 
+#In R, we can fit the logistic regression model with the function glm() (generalized linear models). If we want to compute the conditional probabilities, we want type="response" since the default is to return the logistic transformed values.
+
+heights %>% 
+  mutate(x = round(height)) %>%
+  group_by(x) %>%
+  filter(n() >= 10) %>%
+  summarize(prop = mean(sex == "Female")) %>%
+  ggplot(aes(x, prop)) +
+  geom_point() + 
+  geom_abline(intercept = lm_fit$coef[1], slope = lm_fit$coef[2])
+
+range(p_hat)
+
+# fit logistic regression model
+glm_fit <- train_set %>% 
+  mutate(y = as.numeric(sex == "Female")) %>%
+  glm(y ~ height, data=., family = "binomial")
+
+p_hat_logit <- predict(glm_fit, newdata = test_set, type = "response")
+
+tmp <- heights %>% 
+  mutate(x = round(height)) %>%
+  group_by(x) %>%
+  filter(n() >= 10) %>%
+  summarize(prop = mean(sex == "Female")) 
+logistic_curve <- data.frame(x = seq(min(tmp$x), max(tmp$x))) %>%
+  mutate(p_hat = plogis(glm_fit$coef[1] + glm_fit$coef[2]*x))
+tmp %>% 
+  ggplot(aes(x, prop)) +
+  geom_point() +
+  geom_line(data = logistic_curve, mapping = aes(x, p_hat), lty = 2)
+
+y_hat_logit <- ifelse(p_hat_logit > 0.5, "Female", "Male") %>% factor
+confusionMatrix(y_hat_logit, test_set$sex)$overall[["Accuracy"]]
+
+####2 or 7
+#Key points
+#In this case study we apply logistic regression to classify whether a digit is two or seven. We are interested in estimating a conditional probability that depends on two variables:
+#  g{p(x1,x2}=g{Pr(Y=1|X1=x1,X2=x2)}=β0+β1x1+β2x2 
+#Through this case, we know that logistic regression forces our estimates to be a plane and our boundary to be a line. This implies that a logistic regression approach has no chance of capturing the non-linear nature of the true  p(x1,x2) . Therefore, we need other more flexible methods that permit other shapes.
+
+mnist <- read_mnist()
+is <- mnist_27$index_train[c(which.min(mnist_27$train$x_1), which.max(mnist_27$train$x_1))]
+titles <- c("smallest","largest")
+tmp <- lapply(1:2, function(i){
+  expand.grid(Row=1:28, Column=1:28) %>%
+    mutate(label=titles[i],
+           value = mnist$train$images[is[i],])
+})
+tmp <- Reduce(rbind, tmp)
+tmp %>% ggplot(aes(Row, Column, fill=value)) +
+  geom_raster() +
+  scale_y_reverse() +
+  scale_fill_gradient(low="white", high="black") +
+  facet_grid(.~label) +
+  geom_vline(xintercept = 14.5) +
+  geom_hline(yintercept = 14.5)
+
+data("mnist_27")
+mnist_27$train %>% ggplot(aes(x_1, x_2, color = y)) + geom_point()
+
+is <- mnist_27$index_train[c(which.min(mnist_27$train$x_2), which.max(mnist_27$train$x_2))]
+titles <- c("smallest","largest")
+tmp <- lapply(1:2, function(i){
+  expand.grid(Row=1:28, Column=1:28) %>%
+    mutate(label=titles[i],
+           value = mnist$train$images[is[i],])
+})
+tmp <- Reduce(rbind, tmp)
+tmp %>% ggplot(aes(Row, Column, fill=value)) +
+  geom_raster() +
+  scale_y_reverse() +
+  scale_fill_gradient(low="white", high="black") +
+  facet_grid(.~label) +
+  geom_vline(xintercept = 14.5) +
+  geom_hline(yintercept = 14.5)
+
+fit_glm <- glm(y ~ x_1 + x_2, data=mnist_27$train, family = "binomial")
+p_hat_glm <- predict(fit_glm, mnist_27$test)
+y_hat_glm <- factor(ifelse(p_hat_glm > 0.5, 7, 2))
+confusionMatrix(data = y_hat_glm, reference = mnist_27$test$y)$overall["Accuracy"]
+
+mnist_27$true_p %>% ggplot(aes(x_1, x_2, fill=p)) +
+  geom_raster()
+
+mnist_27$true_p %>% ggplot(aes(x_1, x_2, z=p, fill=p)) +
+  geom_raster() +
+  scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
+  stat_contour(breaks=c(0.5), color="black") 
+
+p_hat <- predict(fit_glm, newdata = mnist_27$true_p)
+mnist_27$true_p %>%
+  mutate(p_hat = p_hat) %>%
+  ggplot(aes(x_1, x_2,  z=p_hat, fill=p_hat)) +
+  geom_raster() +
+  scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
+  stat_contour(breaks=c(0.5),color="black") 
+
+p_hat <- predict(fit_glm, newdata = mnist_27$true_p)
+mnist_27$true_p %>%
+  mutate(p_hat = p_hat) %>%
+  ggplot() +
+  stat_contour(aes(x_1, x_2, z=p_hat), breaks=c(0.5), color="black") +
+  geom_point(mapping = aes(x_1, x_2, color=y), data = mnist_27$test)
+
+##Assessment
+set.seed(1, sample.kind="Rounding") #if you are using R 3.6 or later
+make_data <- function(n = 1000, p = 0.5, 
+                      mu_0 = 0, mu_1=2, 
+                      sigma_0 = 1,  sigma_1 = 1){
+  
+  y <- rbinom(n, 1, p)
+  f_0 <- rnorm(n, mu_0, sigma_0)
+  f_1 <- rnorm(n, mu_1, sigma_1)
+  x <- ifelse(y == 1, f_1, f_0)
+  
+  test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+  
+  list(train = data.frame(x = x, y = as.factor(y)) %>% slice(-test_index),
+       test = data.frame(x = x, y = as.factor(y)) %>% slice(test_index))
+}
+dat <- make_data()
+
+set.seed(1, sample.kind="Rounding")
+delta <- seq(0, 3, len = 25)
+res <- sapply(delta, function(d){
+  dat <- make_data(mu_1 = d)
+  fit_glm <- dat$train %>% glm(y ~ x, family = "binomial", data = .)
+  y_hat_glm <- ifelse(predict(fit_glm, dat$test) > 0.5, 1, 0) %>% factor(levels = c(0, 1))
+  mean(y_hat_glm == dat$test$y)
+})
+qplot(delta, res)
+
+####Smoothing
+#Key points
+#Smoothing is a very powerful technique used all across data analysis. It is designed to detect trends in the presence of noisy data in cases in which the shape of the trend is unknown. 
+#The concepts behind smoothing techniques are extremely useful in machine learning because conditional expectations/probabilities can be thought of as trends of unknown shapes that we need to estimate in the presence of uncertainty.
+
+library(dslabs)
+library(HistData)
+library(tidyverse)
+
+data("polls_2008")
+qplot(day, margin, data = polls_2008)
+
+#Key points
+#The general idea of smoothing is to group data points into strata in which the value of  f(x)  can be assumed to be constant. We can make this assumption because we think  f(x)  changes slowly and, as a result,  f(x)  is almost constant in small windows of time. 
+#This assumption implies that a good estimate for  f(x)  is the average of the  Yi  values in the window. The estimate is:
+#  f^(x0)=1N0∑i∈A0Yi 
+#In smoothing, we call the size of the interval  |x−x0|  satisfying the particular condition the window size, bandwidth or span.
+
+# bin smoothers
+span <- 7 
+fit <- with(polls_2008,ksmooth(day, margin, x.points = day, kernel="box", bandwidth =span))
+polls_2008 %>% mutate(smooth = fit$y) %>%
+  ggplot(aes(day, margin)) +
+  geom_point(size = 3, alpha = .5, color = "grey") + 
+  geom_line(aes(day, smooth), color="red")
+
+# kernel
+span <- 7
+fit <- with(polls_2008, ksmooth(day, margin,  x.points = day, kernel="normal", bandwidth = span))
+polls_2008 %>% mutate(smooth = fit$y) %>%
+  ggplot(aes(day, margin)) +
+  geom_point(size = 3, alpha = .5, color = "grey") + 
+  geom_line(aes(day, smooth), color="red")
