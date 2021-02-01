419c3c876d83dfc6701ba26ed6f01898e9a15a72
edce1987 <44676946+edce1987@users.noreply.github.com>
2021-02-01 16:57:52 +0100

    Update Final Project

--- 09_Capstone/Movielens/edcem_MovieLens.R
+++ 09_Capstone/Movielens/edcem_MovieLens.R
@@ -61,12 +61,12 @@ library(lubridate)
 
 ##Data Wrangling
 #Assumption: The timestamp which indicates when a certain movie was rated by a certain user has an effect on the movie rating.
-#Convert timestamp to datetime and then to day.
+#Convert timestamp to datetime and then to day to have a granular view.
 edxT <- edx %>% mutate(date = round_date(as_datetime(timestamp), unit = "day"))
 dim(edxT)
 
 #Assumption: Users that rate more often have more experience and therefore a better judgment which will reduce RMSE.
-edxT <- edxT %>% group_by(userId) %>% filter(n() >= 50) %>% ungroup()
+edxT <- edxT %>% group_by(userId) %>% filter(n() >= 25) %>% ungroup()
 dim(edxT)
 
 #Split edx data into train and test set
@@ -78,27 +78,31 @@ testIndex <- createDataPartition(y = edxT$rating, times = 1, p = 0.2, list = FAL
 trainSet <- edxT[-testIndex, ]
 testSet <- edxT[testIndex, ]
 
-#Make sure UserId and MovieId are existing in train and test set
+#Make sure UserId and MovieId are existing in train and test set by applying semi_join.
 testSet <- testSet %>% 
   semi_join(trainSet, by = "movieId") %>%
   semi_join(trainSet, by = "userId")
 
-#Define function to compute Root Mean Squared Error (RMSE) (with removal of NaNs) based on true outcome and the prediction.
+#Define function to compute Root Mean Squared Error (RMSE) (with removal of NAs) 
+#based on true outcome and the prediction.
 RMSE <- function(true, predicted){
   sqrt(mean((true - predicted)^2, na.rm = TRUE))
 }
 
 #Model name: Regularized Model with Movie, User, Time & Genre Effect.
-#Find optimal lambda for the model.
-#I recommend not running this code since it will take hours.
+#Find optimal lambda for the model. I have narrowed the range to avoid excessive 
+#computing time. From my trials I narrowed down the optimal area somewhere between
+#4.9 to 5.15 in 0.001 steps which still equals 250 iterations.
+#I recommend not running this code since it will take very long depending on your
+#hardware.
 lambdas <- seq(4.9, 5.15, 0.001)
 rmses <- sapply(lambdas, function(l){
-  mu <- mean(trainSet$rating) #Feature Composition: Average movie rating mu
-  b_i <- trainSet %>% #Feature Composition: Regularized Movie Effect b_i
+  avg <- mean(trainSet$rating) #Feature Composition: Average movie rating mu
+  b_movie <- trainSet %>% #Feature Composition: Regularized Movie Effect b_i
     group_by(movieId) %>%
-    summarize(b_i = sum(rating - mu)/(n()+l))
-  b_u <- trainSet %>% #Feature Composition: Regularized User Effect b_u
-    left_join(b_i, by="movieId") %>%
+    summarize(b_movie = sum(rating - avg)/(n()+l))
+  b_user <- trainSet %>% #Feature Composition: Regularized User Effect b_u
+    left_join(b_movie, by="movieId") %>%
     group_by(userId) %>%
     summarize(b_u = sum(rating - mu - b_i)/(n()+l))
   b_t <- trainSet %>% #Feature Composition: Regularized Time Effect b_t
@@ -124,69 +128,75 @@ rmses <- sapply(lambdas, function(l){
 
 #Find optimal lambda where RMSE is minimized.
 lambda <- lambdas[which.min(rmses)]
+
+#Print optimal lambda
 lambda
 
 #Perform evaluation with optimal lambda.
 rmse <- sapply(lambda, function(l){
-  mu <- mean(trainSet$rating) #Feature Composition: Average movie rating mu
-  b_i <- trainSet %>% #Feature Composition: Regularized Movie Effect b_i
+  avg <- mean(trainSet$rating) #Feature Composition: Average movie rating mu
+  b_movie <- trainSet %>% #Feature Composition: Regularized Movie Effect b_i
     group_by(movieId) %>%
-    summarize(b_i = sum(rating - mu)/(n()+l))
-  b_u <- trainSet %>% #Feature Composition: Regularized User Effect b_u
-    left_join(b_i, by="movieId") %>%
+    summarize(b_movie = sum(rating - mu)/(n()+l))
+  b_user <- trainSet %>% #Feature Composition: Regularized User Effect b_u
+    left_join(b_movie, by="movieId") %>%
     group_by(userId) %>%
-    summarize(b_u = sum(rating - mu - b_i)/(n()+l))
-  b_t <- trainSet %>% #Feature Composition: Regularized Time Effect b_t
-    left_join(b_i, by='movieId') %>%
-    left_join(b_u, by='userId') %>% 
+    summarize(b_user = sum(rating - avg - b_movie)/(n()+l))
+  b_time <- trainSet %>% #Feature Composition: Regularized Time Effect b_t
+    left_join(b_movie, by='movieId') %>%
+    left_join(b_user, by='userId') %>% 
     group_by(date) %>% 
-    summarize(b_t = sum(rating - mu - b_i - b_u)/(n()+l))
-  b_g <- trainSet %>% #Feature Composition: Regularized Genre Effect b_g
-    left_join(b_i, by='movieId') %>%
-    left_join(b_u, by='userId') %>%
-    left_join(b_t, by='date') %>%
+    summarize(b_time = sum(rating - avg - b_movie - b_user)/(n()+l))
+  b_genre <- trainSet %>% #Feature Composition: Regularized Genre Effect b_g
+    left_join(b_movie, by='movieId') %>%
+    left_join(b_user, by='userId') %>%
+    left_join(b_time, by='date') %>%
     group_by(genres) %>%
-    summarize(b_g = sum(rating - mu - b_i - b_u - b_t)/(n()+l))
+    summarize(b_genre = sum(rating - avg - b_movie - b_user - b_time)/(n()+l))
   predicted_ratings <- testSet %>% #Perform prediction based on feature composition
-    left_join(b_i, by = "movieId") %>%
-    left_join(b_u, by = "userId") %>%
-    left_join(b_t, by = "date") %>%
-    left_join(b_g, by = "genres") %>%
-    mutate(pred = mu + b_i + b_u + b_t + b_g) %>%
+    left_join(b_movie, by = "movieId") %>%
+    left_join(b_user, by = "userId") %>%
+    left_join(b_time, by = "date") %>%
+    left_join(b_genre, by = "genres") %>%
+    mutate(pred = avg + b_movie + b_user + b_time + b_genre) %>%
+    mutate(pred = ifelse(is.na(pred), avg, pred)) %>%
     .$pred
   return(RMSE(predicted_ratings, testSet$rating))
 })
+
+#Print rmse
 rmse
 
 #Model name: Regularized Model with Movie, User, Time & Genre Effect.
 #Final evaluation of validation set with final algorithm.
 finalRmse <- sapply(lambda, function(l){
-  mu <- mean(trainSet$rating) #Feature Composition: Average movie rating mu.
-  b_i <- trainSet %>% 
+  avg <- mean(trainSet$rating) #Feature Composition: Average movie rating mu.
+  b_movie <- trainSet %>% 
     group_by(movieId) %>%
-    summarize(b_i = sum(rating - mu)/(n()+l)) #Feature Composition: Regularized Movie Effect b_i.
-  b_u <- trainSet %>% 
-    left_join(b_i, by="movieId") %>%
+    summarize(b_movie = sum(rating - avg)/(n()+l)) #Feature Composition: Regularized Movie Effect b_i.
+  b_user <- trainSet %>% 
+    left_join(b_movie, by="movieId") %>%
     group_by(userId) %>% 
-    summarize(b_u = sum(rating - mu - b_i)/(n()+l)) #Feature Composition: Regularized User Effect b_u.
-  b_t <- trainSet %>% 
-    left_join(b_i, by='movieId') %>%
-    left_join(b_u, by='userId') %>% 
+    summarize(b_user = sum(rating - avg - b_movie)/(n()+l)) #Feature Composition: Regularized User Effect b_u.
+  b_time <- trainSet %>% 
+    left_join(b_movie, by='movieId') %>%
+    left_join(b_user, by='userId') %>% 
     group_by(date) %>% 
-    summarize(b_t = sum(rating - mu - b_i - b_u)/(n()+l)) #Feature Composition: Regularized Time Effect b_t.
-  b_g <- trainSet %>% 
-    left_join(b_i, by='movieId') %>%
-    left_join(b_u, by='userId') %>%
-    left_join(b_t, by='date') %>%
+    summarize(b_time = sum(rating - avg - b_movie - b_user)/(n()+l)) #Feature Composition: Regularized Time Effect b_t.
+  b_genre <- trainSet %>% 
+    left_join(b_movie, by='movieId') %>%
+    left_join(b_user, by='userId') %>%
+    left_join(b_time, by='date') %>%
     group_by(genres) %>%
-    summarize(b_g = sum(rating - mu - b_i - b_u - b_t)/(n()+l)) #Feature Composition: Regularized Genre Effect b_g.
+    summarize(b_genre = sum(rating - avg - b_movie - b_user - b_time)/(n()+l)) #Feature Composition: Regularized Genre Effect b_g.
   predicted_ratings <- validation %>% 
     mutate(date = as_datetime(timestamp), date = round_date(date, unit = "day")) %>%  #Required date feature is created.
-    left_join(b_i, by = "movieId") %>%
-    left_join(b_u, by = "userId") %>%
-    left_join(b_t, by = "date") %>%
-    left_join(b_g, by = "genres") %>%
-    mutate(pred = mu + b_i + b_u + b_t + b_g) %>% #Perform prediction based on feature composition.
+    left_join(b_movie, by = "movieId") %>%
+    left_join(b_user, by = "userId") %>%
+    left_join(b_time, by = "date") %>%
+    left_join(b_genre, by = "genres") %>%
+    mutate(pred = avg + b_movie + b_user + b_time + b_genre) %>% #Perform prediction based on feature composition.
+    mutate(pred = ifelse(is.na(pred), avg, pred)) %>% #Since NAs can be prodocued due to our data restrictions, those are filled with the overal average mu.
     .$pred #Pull predictions. 
   return(RMSE(predicted_ratings, validation$rating)) #Calculate RMSE.
 })
